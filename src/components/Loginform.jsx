d29048fd7e37d471bcd5a5ebb08732b39dd9c00c
Hania <haniaaa0987@example.com>
2024-07-23 17:13:42 +0500

    add

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const LoginForm = () => {
+    const mystate= useContext(AuthContext);
+    console.log("login", mystate);
+    console.log("useryyy", mystate.username);
+
+
+    const handleClick = (e) => {
+        e.preventDefault(); // Prevents the default form submission behavior
+        console.log('Login button clicked');
+        console.log('Username:', mystate.username);
+        console.log('Password:', mystate.password);
+
+        
+
+
+
+        // Add any other functionality you want to perform on button click here
+      };
+    
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-100 to-slate-300">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-2xl font-bold mb-6 text-gray-900 text-center">Login</h2>
         <form>
           <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700 font-medium mb-2">Username</label>
+            <label htmlFor="username " className="block text-gray-700 font-medium mb-2">Username </label>
             <input
               type="text"
               id="username"
+              value={mystate.username}
+              onChange={(e) => mystate.setUsername(e.target.value)}
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
               placeholder="Enter your username"
             />
@@ -20,12 +43,15 @@ const LoginForm = () => {
             <input
               type="password"
               id="password"
+              value={mystate.password}
+              onChange={(e) => mystate.setPassword(e.target.value)}
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
               placeholder="Enter your password"
             />
           </div>
           <button
             type="submit"
+            onClick={handleClick}
             className="w-full bg-blue-500 text-white font-medium py-3 rounded-lg hover:bg-blue-600 transition duration-300"
           >
             Login
